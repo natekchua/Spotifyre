83dd84c42477b62d6b69b4795e72e9bdf516cfb5
Nathan Chua <natekchua@gmail.com>
2020-11-22 17:32:36 -0700

    added Login page and spotify authentication

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login/Login';
+
+function App() {
+  return (
+    <div className='app'>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.css';
+import spotifyreLogo from '../../icons/spotifyre.png';
+import { loginURL } from '../../services/spotify';
+
+function Login() {
+  return (
+    <div className='login-page flex-basic'>
+      <div className='title flex-basic'>
+        <img src={spotifyreLogo} alt='logo'/>
+        <h1>Spotifyre</h1>
+      </div>
+      <h2>A Spotify Manager for Playlist Curators</h2>
+      <a href={loginURL}>Login with Spotify</a>
+    </div>
+  )
+}
+
+export default Login;
