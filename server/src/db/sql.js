0e93293440f291010ce133dcb0674133aeef53f9
raksdewji <rakheemdewji@hotmail.com>
2020-11-24 20:28:21 -0700

    intial backend setup

---
+++ server/src/db/sql.js
@@ -0,0 +1,18 @@
+// Connect with PostgresSQL
+const pg = require('pg');
+
+// Database config
+const config = {
+  user: process.env.DB_user,
+  database: process.env.DB,
+  password: process.env.DB_password,
+  host: process.env.DB_host,
+  port: 5432,
+  ssl=true,
+
+  poolSize: 5,
+  poolIdleTimeout: 30000,
+  reapIntervalMillis: 10000,
+};
+
+const pool = new pg.Pool(config);
