46915e57b1b041e9b96cee41ac0ddf1f298d5341
Daniel <daniel.artuso1@gmail.com>
2020-11-29 15:44:29 -0700

    made search flexible

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from 'react'
-import SearchIcon from '@material-ui/icons/Search';
-import './Search.css.css';
+import React from 'react'
+import './Search.css';
+import TextField from '@material-ui/core/TextField'
+import SearchIcon from '@material-ui/icons/Search'
+import InputAdornment from '@material-ui/core/InputAdornment'
 
-function Search () {
-  const [message, setMessage] = useState("");
-
-  function sendMessage (event) {
-    event.preventDefault()
-    if (message !== "") {
-      setMessage("")
-      //TODO:call spotify api
-    }
+/*Search component
+*
+* requires:
+* message: "" or default message
+* setMessage: updates stored message on change function (generally setMessage from hook setState)
+* sendMessage: functions that handles request to search by user (recommend to event.ignoreDefault)
+* searchMessage: text displayed in search box, defaults to search spotifyre if not passed
+*
+* uses:
+* setMessage(event): call setMessage on changes to update message
+* sendMessage(event): on Enter keypress or Icon click calls sendMessage
+* */
+function Search ({message,setMessage,sendMessage, searchMessage}) {
+  if(searchMessage == null){
+    searchMessage = "Search Spotifyre"
   }
 
+
   return (
-    <form className='search'>
-      <input className="inputBox" type="text" placeholder="Search Spotify" value={message}
-             onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-             onChange={({target: {value}}) => setMessage(value)}
-      />
-      <SearchIcon/>
+    <form className='search' noValidate autoComplete="off">
+      <TextField id="filled-basic" label={searchMessage} variant="filled" value={message}
+                 onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                 onChange={({target: {value}}) => setMessage(value)}
+                 InputProps={{
+                   endAdornment: <InputAdornment position="end">
+                     <SearchIcon onClick={event => sendMessage(event)}/>
+                   </InputAdornment>
+                 }}
+        />
     </form>
   );
 }
