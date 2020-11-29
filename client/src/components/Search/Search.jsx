133e602c2bbb9106b04640b66b9b4d2e7f796d34
Daniel <daniel.artuso1@gmail.com>
2020-11-29 16:04:49 -0700

    styling

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Search.css';
+import './Search.css'
 import TextField from '@material-ui/core/TextField'
 import SearchIcon from '@material-ui/icons/Search'
 import InputAdornment from '@material-ui/core/InputAdornment'
@@ -16,25 +16,24 @@ import InputAdornment from '@material-ui/core/InputAdornment'
 * setMessage(event): call setMessage on changes to update message
 * sendMessage(event): on Enter keypress or Icon click calls sendMessage
 * */
-function Search ({message,setMessage,sendMessage, searchMessage}) {
-  if(searchMessage == null){
-    searchMessage = "Search Spotifyre"
+function Search ({ message, setMessage, sendMessage, searchMessage }) {
+  if (searchMessage == null) {
+    searchMessage = 'Search Spotifyre'
   }
-
 
   return (
     <form className='search' noValidate autoComplete="off">
       <TextField id="filled-basic" label={searchMessage} variant="filled" value={message}
                  onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-                 onChange={({target: {value}}) => setMessage(value)}
+                 onChange={({ target: { value } }) => setMessage(value)}
                  InputProps={{
                    endAdornment: <InputAdornment position="end">
                      <SearchIcon onClick={event => sendMessage(event)}/>
                    </InputAdornment>
                  }}
-        />
+      />
     </form>
-  );
+  )
 }
 
-export default Search;
+export default Search
