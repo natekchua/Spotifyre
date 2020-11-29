10357d825341e2e8ca2cce67309bde2971343ca5
Daniel <daniel.artuso1@gmail.com>
2020-11-29 16:15:52 -0700

    basic structure

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect } from 'react';
-import { useProviderValue } from '../ContextState/Provider';
-
-import './Dashboard.css';
+import React, { useEffect } from 'react'
+import { useProviderValue } from '../ContextState/Provider'
+import './Dashboard.css'
 
 function Dashboard () {
-  const [{
-    tab
-  }, dispatch] = useProviderValue();
+  const [dispatch] = useProviderValue()
 
   useEffect(() => {
     dispatch({
@@ -16,10 +13,27 @@ function Dashboard () {
   }, [])
 
   return (
-    <div>
-      Welcome to your {tab}!
-    </div>
-  );
+    <>
+      <div>
+        Top Playlist
+        <div>
+          {/*TODO:get top playlist from database? */}
+        </div>
+      </div>
+      <div>
+        Recommended Playlists
+        <div>
+          {/*TODO: get suggested playlist*/}
+        </div>
+      </div>
+      <div>
+        Recently Played Songs
+        <div>
+          {/*TODO:get recently played songs from spotify */}
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
