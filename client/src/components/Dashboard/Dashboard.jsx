77ca053101a948fc102fcf5d6ddbf94f179fae59
Daniel <daniel.artuso1@gmail.com>
2020-12-02 01:02:06 -0700

    more structure

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,16 +1,45 @@
 import React, { useEffect } from 'react'
 import { useProviderValue } from '../ContextState/Provider'
 import './Dashboard.css'
+import PlaylistOption from '../Sidebar/Option/PlaylistOption'
 
-function Dashboard () {
-  const [dispatch] = useProviderValue()
+function Dashboard (props) {
+  // const [dispatch] = useProviderValue()
+  const { spotify } = props
+  const [{ playlists }, dispatch] = useProviderValue()
+
+  const selectPlaylist = (id) => {
+    spotify.getPlaylist(id).then(playlist => {
+      dispatch({
+        type: 'SET_CURR_PLAYLIST',
+        currPlaylist: playlist
+      })
+    })
+  }
 
   useEffect(() => {
     dispatch({
       type: 'SET_TAB',
       tab: 'Dashboard'
     })
+
+    //  TODO: get recently played and curator playlists to display
+
   }, [])
+
+  const playlistOptions = playlists?.items?.map((p, idx) =>
+    <PlaylistOption
+      key={idx}
+      optionName={p.name}
+      playlist={p}
+      onSelectPlaylist={selectPlaylist}
+    />
+  )
+
+  const recentlyPlayed = () => {
+    //  https://developer.spotify.com/documentation/web-api/reference/player/get-recently-played/
+    //   const songs = currPlaylist?.tracks.items.map((s, idx) => <Song key={idx} song={s.track} onPlaySong={playSong} />)
+  }
 
   return (
     <>
