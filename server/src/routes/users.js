4fc2c17177e5573ffe9e9c3abd7575da7ce1b9d0
Rakheem Dewji <rakheemdewji@hotmail.com>
2020-12-02 23:48:43 -0700

    SQL Database Backend (#30)
    * added backend files for api calls and sql queries
    
    * removed unnecessary module exports
    
    * feat(backend): added increase/decrease function
    
    * small typo

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,9 +1,136 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const action = require('../actions/actions.js');
+const { SQL } = require('../db/sql.js');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+
+router.get('/getCurators', (req, res) => {
+  action
+    .getAllCurators()
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.get('/getType', (req, res) => {
+  action
+    .getType()
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.get('/getPlaylistID', (req, res) => {
+  const userID = req.body;
+
+  action
+    .getPlaylistID(userID)
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.get('/getAllPlaylists', (req, res) => {
+  action
+    .getAllPlaylists()
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.get('/getSuggestions', (req, res) => {
+  const playlistID = req.body; // Need to define a paramter
+
+  action
+    .getSuggestions(playlistID)
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.post('/addSongSuggestion', (req, res) => {
+  const { playlistID, songID, suggestedUserID, playlist, count } = req.body;
+
+  action
+    .addSongSuggestion(playlistID, songID, suggestedUserID, playlist, count)
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.post('/removeSong', (req, res) => {
+  const { playlistID, songID } = req.body;
+
+  action
+    .removeSong(playlistID, songID)
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.post('/increaseCount', (req, res) => {
+  const playlistID = req.body;
+
+  action
+    .increaseCount(playlistID)
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
+});
+
+router.post('/increaseCount', (req, res) => {
+  const playlistID = req.body;
+
+  action
+    .decreaseCount(playlistID)
+    .then((rtn) => {
+      res.send(rtn);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.send({ error: err });
+    });
 });
 
 module.exports = router;
