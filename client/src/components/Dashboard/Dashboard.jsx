d2553869912af17a3fe97df6ad8cfb72247cb1e6
Daniel <daniel.artuso1@gmail.com>
2020-12-03 13:55:56 -0700

    updated structure
    Signed-off-by: Daniel <daniel.artuso1@gmail.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,59 +1,35 @@
 import React, { useEffect } from 'react'
 import { useProviderValue } from '../ContextState/Provider'
 import './Dashboard.css'
-import PlaylistOption from '../Sidebar/Option/PlaylistOption'
-import { selectPlaylist } from '../../services/apiRequests'
+import PlaylistDisplay from './PlaylistDisplay/PlaylistDisplay'
+import RecentlyPlayed from './RecentlyPlayed/RecentlyPlayedList'
 
-function Dashboard (props) {
-  // const [dispatch] = useProviderValue()
-  const { spotify } = props
-  const [{ playlists }, dispatch] = useProviderValue()
-
-  const onSelectPlaylist = (id) => {
-    selectPlaylist(id).then(res => {
-      dispatch({
-        type: 'SET_CURR_PLAYLIST',
-        currPlaylist: JSON.parse(res).playlist
-      })
-    }).catch(err => console.log(err))
-  }
+function Dashboard () {
+  const [{ playOnRepeat, playRewind, playCapsule }, dispatch] = useProviderValue()
 
   useEffect(() => {
     dispatch({
       type: 'SET_TAB',
       tab: 'Dashboard'
     })
-
-    //  TODO: get recently played and curator playlists to display
-
   }, [])
 
-  const playlistOptions = playlists?.items?.map((p, idx) =>
-    <PlaylistOption
-      key={idx}
-      optionName={p.name}
-      playlist={p}
-      onSelectPlaylist={selectPlaylist}
-    />
-  )
 
-  const recentlyPlayed = () => {
-    //  https://developer.spotify.com/documentation/web-api/reference/player/get-recently-played/
-    //   const songs = currPlaylist?.tracks.items.map((s, idx) => <Song key={idx} song={s.track} onPlaySong={playSong} />)
-  }
 
   return (
     <>
       <div>
         Top Playlist
         <div>
-          {/*TODO:get 3 default albums */}
+          <PlaylistDisplay playlist={playOnRepeat}/>
+          <PlaylistDisplay playlist={playRewind}/>
+          <PlaylistDisplay playlist={playCapsule}/>
         </div>
       </div>
       <div>
         Recently Played Songs
         <div>
-          {/*TODO:get recently played songs from spotify */}
+          <RecentlyPlayed/>
         </div>
       </div>
     </>
