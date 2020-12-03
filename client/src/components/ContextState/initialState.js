0fc76d61774fb8a274ca2c1be8996b5c9949995c
Daniel <daniel.artuso1@gmail.com>
2020-12-03 13:57:17 -0700

    added recentsongs item to fill from api
    Signed-off-by: Daniel <daniel.artuso1@gmail.com>

--- client/src/components/ContextState/initialState.js
+++ client/src/components/ContextState/initialState.js
@@ -19,4 +19,5 @@ export const initialState = {
   playOnRepeat: '37i9dQZF1EphOfEPXi5KfT',
   playRewind: '37i9dQZF1EpDfnKhctSw35',
   playCapsule: '37i9dQZF1EuIxSCUbcUAge',
+  recentSongs: null,
 };
