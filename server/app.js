324acbf26abc43c35f05321a4a1d8731acb8670f
Nathan Chua <natekchua@gmail.com>
2020-12-10 00:20:42 -0700

    serve index.html from the backend

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const path = require('path');
 
 const indexRouter = require('./src/routes/index');
 const usersRouter = require('./src/routes/users');
@@ -14,6 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
