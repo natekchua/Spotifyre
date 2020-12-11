fa22da962cc0e258bba3f76ab346f4bdcb13ba39
Nathan Chua <natekchua@gmail.com>
2020-12-10 17:21:21 -0700

    commented cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
