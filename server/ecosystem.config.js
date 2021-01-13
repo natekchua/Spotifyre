f4ca2bad1bb2f178000cfc9b0a3b272d2d7c625d
awave1 <artgolovin96@gmail.com>
2021-01-12 23:01:05 -0700

    fix(server): force cluster mode and fix start script

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: 'ts-node',
       args: './src/index.ts',
       instances: '1',
+      exec_mode: 'cluster',
       env_production: {
         NODE_ENV: 'production'
       }
